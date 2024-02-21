3a17cea21f462430e8e1e2322f8bb641a2bda194
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2024-02-21 08:29:00 +0630

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const List = ({task,toggleTask,removeTask}) => {
+const List = ({ task, toggleTask, toggleFavourite, removeTask }) => {
     return (
         <li key={task.id}>
             <input
@@ -9,9 +9,12 @@ const List = ({task,toggleTask,removeTask}) => {
                 onChange={() => toggleTask(task.id)}
             />
             <p className='todoText' style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
-            <span className='timeDue'>{task.time}</span>{task.text}
+                <span className={`timeDue`} style={{ background: task.color}}>
+                    {task.time}
+                </span>
+                <span style={{ color: task.color}}>{task.category}</span> : {task.text}
             </p>
-            <button onClick={() => removeTask(task.id)}>
+            <button className={task.favourite ? 'favourited' : ''} onClick={() => toggleFavourite(task.id)}>
                 <span className="material-symbols-outlined">
                     favorite
                 </span>
