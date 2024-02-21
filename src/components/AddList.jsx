0da7a11c161c75036701de89dd2d3a800f571cb7
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2024-02-21 08:28:43 +0630

    Update AddList.jsx

--- src/components/AddList.jsx
+++ src/components/AddList.jsx
@@ -1,25 +1,43 @@
 import React from 'react'
 
-const AddList = ({currentTime,newTask,setNewTask,setNewTime,addTask}) => {
+const AddList = ({ options ,selectedValue,handleSelectChange,currentTime, newTask, setNewTask, setNewTime, addTask }) => {
+  const handleTimeChange = (event) => {
+    const newTime = event.target.value;
+    setNewTime(newTime);
+  };
+
   return (
     <div className='addListBox'>
-        <input
-          type="text"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          placeholder="Enter a new task"
-        />
-        <input 
-        type="time" 
-        name="dueTime" 
-        onChange={(e) => setNewTime(e.target.value)}
-        value={currentTime}
-        id="dueTime" />
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Enter a new task"
+      />
+
+      <div>
+        <div>
+          <input
+            type="time"
+            name="dueTime"
+            onChange={handleTimeChange}
+            value={currentTime}
+            id="dueTime" />
+          <select id="selectBox" value={selectedValue} onChange={handleSelectChange}>
+            {options.map((option, index) => (
+              <option key={index} value={option.name}>
+                {option.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
         <button onClick={addTask}>
-            <span className="material-symbols-outlined">
-                add
-            </span>
+          <span className="material-symbols-outlined">
+            add
+          </span>
         </button>
+      </div>
     </div>
   )
 }
