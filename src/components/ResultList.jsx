edaf1e9d5f0c2210f8d4b8dab7947141ef694a80
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2024-02-21 08:29:31 +0630

    Update ResultList.jsx

--- src/components/ResultList.jsx
+++ src/components/ResultList.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import List from './List'
 
-const ResultList = ({ tasks, toggleTask, removeTask }) => {
+const ResultList = ({ tasks, toggleTask,toggleFavourite, removeTask }) => {
     return (
         <div className='resultListBox'>
             <ul>
                 {tasks.map((task, index) => (
-                    <List key={index} task={task} toggleTask={toggleTask} removeTask={removeTask} />
+                    <List key={index} task={task} toggleTask={toggleTask} toggleFavourite={toggleFavourite} removeTask={removeTask} />
                 ))}
             </ul>
         </div>
